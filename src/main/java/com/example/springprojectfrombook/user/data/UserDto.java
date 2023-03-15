da74497fb28c20efd42fee8cafce9d1242953797
shoh99 <46786219+shoh99@users.noreply.github.com>
2023-03-16 01:59:45 +0900

    Added UserDto class

---
+++ src/main/java/com/example/springprojectfrombook/user/data/UserDto.java
@@ -0,0 +1,17 @@
+package com.example.springprojectfrombook.user.data;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private UUID id;
+    private String email;
+    private String password;
+}
