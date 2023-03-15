f7578c89e1dd850033505672bbd5ce6ebee2e284
shoh99 <46786219+shoh99@users.noreply.github.com>
2023-03-16 01:16:03 +0900

    Added Security configurations, And Created new User repository and entity

--- src/main/java/com/example/springprojectfrombook/config/SecurityConfig.java
+++ src/main/java/com/example/springprojectfrombook/config/SecurityConfig.java
@@ -1,14 +1,20 @@
 package com.example.springprojectfrombook.config;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
+@AllArgsConstructor
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -16,9 +22,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // place customized userDetailService here
     }
 
-@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
-@Override
-public AuthenticationManager authenticationManagerBean() throws Exception{
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
         return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                // first chain
+                .csrf()
+                .disable()
+                // second chain
+                .antMatcher("/**")
+                .authorizeRequests()
+                // third chain
+                .antMatchers("/**")
+                .permitAll()
+                // fourth chain
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS
+                );
+
 }
+
 }
