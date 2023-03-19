941e67aa4db1f19aa49faa99660e841c30655089
shoh99 <46786219+shoh99@users.noreply.github.com>
2023-03-20 00:13:52 +0900

    fixed bugs at jwtUtil file

--- src/main/java/com/example/springprojectfrombook/jwt/models/UserPrincipal.java
+++ src/main/java/com/example/springprojectfrombook/jwt/models/UserPrincipal.java
@@ -14,7 +14,6 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        getAuthorities();
         return null;
     }
 
